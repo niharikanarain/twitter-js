49533d5e22e0c060609e32a4ca8cb3dfa5653f97
lcbuchanan <buchanan.lori@gmail.com>
2017-11-06 16:59:58 -0600

    lori making changes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
-const port = 3000; 
+const port = 3000;
 
 app.use((req, res,next) => {
-    console.log(req.method, req.path, res.status); 
-    next(); 
+    console.log(req.method, req.path, res.status);
+    next();
 })
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello'); 
-}); 
+    res.status(200).send('Hello');
+});
 
 
-
+//this is lori's test comment!
 
 app.listen(port, () => {
-    console.log("server listening"); 
+    console.log("server listening");
 })
