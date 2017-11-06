c04009ee5c8aad7b4e86b0bbbd3016af8361be73
Niharika Narain <niharikanarain@gmail.com>
2017-11-06 17:36:06 -0600

    got our nunjucks example template to show up

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const app = express(); // creates an instance of an express application
 const port = 3000;
 
 app.use((req, res,next) => {
-    console.log(req.method, req.path, res.status);
+    console.log(req.method, req.path);
     next();
 })
 
+nj.configure('views', {
+    noCache: true
+}); 
+
 app.get('/', (req, res) => {
-    res.status(200).send('Hello');
+    nj.render('index.html', {
+        title: "An Example",
+        people: [ {name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}]
+    }, function (err, output){
+        res.send(output); 
+        if (err) console.log(err); 
+        console.log("OUTPUT: ",output); 
+    }); 
+    // res.status(200).send('Hello');
 });
-
-
-//this is lori's test comment!
 
 app.listen(port, () => {
     console.log("server listening");
