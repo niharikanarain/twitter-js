ad793673b3f85663f04aeaf02b5d795bd1586992
Niharika Narain <niharikanarain@gmail.com>
2017-11-07 09:12:31 -0600

    refactored nunjucks template

--- app.js
+++ app.js
@@ -8,21 +8,29 @@ app.use((req, res,next) => {
     next();
 })
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nj.render); // when giving html files to res.render, tell it to use nunjucks
 nj.configure('views', {
     noCache: true
-}); 
+}); // point nunjucks to the proper directory for templates
+
 
 app.get('/', (req, res) => {
-    nj.render('index.html', {
-        title: "An Example",
-        people: [ {name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}]
-    }, function (err, output){
-        res.send(output); 
-        if (err) console.log(err); 
-        console.log("OUTPUT: ",output); 
-    }); 
+    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
+    // nj.render('index.html', {
+    //     title: "An Example",
+    //     people: [ {name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}]
+    // }, function (err, output){
+    //     res.send(output); 
+    //     if (err) console.log(err); 
+    //     console.log("OUTPUT: ",output); 
+    // }); 
     // res.status(200).send('Hello');
 });
+
+
+
 
 app.listen(port, () => {
     console.log("server listening");
